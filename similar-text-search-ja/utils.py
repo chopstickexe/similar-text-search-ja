fd95937570503f1f8d97f0658630cfd517c008f9
Risa Nishiyama <chopstickexe@users.noreply.github.com>
2020-11-04 22:58:40 +0900

    Convert config.ini to config.json (#3)
    * Re-implement utils
    
    * Replace ini file to json
    
    * Use config.json

--- similar-text-search-ja/utils.py
+++ similar-text-search-ja/utils.py
@@ -1,17 +1,36 @@
-import configparser
-import logging.config
-import os
+import json
+from logging import Formatter, StreamHandler, getLogger
+from pathlib import Path
+from typing import Any, Dict
 
 
-def get_package_root():
-    return os.path.dirname(__file__)
+def get_dir(file: str = __file__) -> Path:
+    """Return path to the directory of the given file
+
+    Args:
+        file (str, optional): Python script or other types of file. Defaults to __file__.
+
+    Returns:
+        Path: directory path
+    """
+    return Path(file).parent
 
 
-def read_log_config():
-    logging.config.fileConfig(os.path.join(get_package_root(), "logging.ini"))
+def set_root_logger(level: str = "INFO"):
+    """Set formatters and handlers to the root logger
+
+    Args:
+        level (str, optional): logging level. Defaults to "INFO".
+    """
+    logger = getLogger()
+    formatter = Formatter("%(asctime)s %(name)-14s %(levelname)-8s %(message)s")
+    handler = StreamHandler()
+    handler.setLevel(level)
+    handler.setFormatter(formatter)
+    logger.setLevel(level)
+    logger.addHandler(handler)
 
 
-def get_app_config() -> configparser.ConfigParser:
-    app_conf = configparser.ConfigParser()
-    app_conf.read(os.path.join(get_package_root(), "config.ini"))
-    return app_conf
+def read_json_config(config_file: Path) -> Dict[str, Any]:
+    with open(str(config_file), "r") as f:
+        return json.load(f)
