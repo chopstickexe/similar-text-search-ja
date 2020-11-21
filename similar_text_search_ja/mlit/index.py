3af316d4c7c57d538255071510e2d27fc0ad639e
Risa Nishiyama <lisa.nishiyama@gmail.com>
2020-11-21 14:50:07 +0900

    Separate general indexing and MLIT specific code

--- similar_text_search_ja/mlit/index.py
+++ similar_text_search_ja/mlit/index.py
@@ -1,81 +1,6 @@
-import logging
-from typing import Any, Dict, List
-
-import csv_parser
-import es as es_wrapper
-import utils, vectorizers
-
-
-def create_index(
-    es: "es_wrapper.ES",
-    clear_index: bool,
-    index: str,
-    date_field: str,
-    date_format: str,
-    bert_cls_field: str,
-    bert_vec_size: int,
-):
-    if clear_index:
-        es.delete_index(index)
-
-    es.create_index(
-        index,
-        body={
-            "mappings": {
-                "properties": {
-                    date_field: {"type": "date", "format": date_format},
-                    bert_cls_field: {"type": "dense_vector", "dims": bert_vec_size},
-                }
-            }
-        },
-    )
-
-
-def get_target_fields_txt(doc: Dict[str, Any], target_fields: List[str]) -> str:
-    ret = " ".join(
-        [doc[field] for field in target_fields if field in doc and doc[field]]
-    )
-    return ret
-
-
-def vectorize_doc(batch_docs, target_fields, bert_cls_field, vectorizer):
-    if not batch_docs or len(batch_docs) == 0:
-        return
-    batch_txts = [get_target_fields_txt(doc, target_fields) for doc in batch_docs]
-    input_ids = vectorizer.encode(batch_txts)
-    outputs = vectorizer.vectorize(input_ids)
-    assert len(outputs.last_hidden_state) == len(batch_docs)
-    for i, doc in enumerate(batch_docs):
-        doc[bert_cls_field] = outputs.last_hidden_state[i][0][:].tolist()
-
-
-def get_documents(
-    csv_path: str,
-    target_fields: List[str],
-    bert_cls_field: str,
-    vectorizer: "vectorizers.JaVectorizer",
-) -> List[Dict[str, Any]]:
-    logger = logging.getLogger(__name__)
-    ret = []
-    with csv_parser.CsvParser(csv_path) as parser:
-        batch_docs = []
-        for doc in parser:
-            batch_docs.append(doc)
-            if len(batch_docs) == 32:
-                vectorize_doc(batch_docs, target_fields, bert_cls_field, vectorizer)
-                ret.extend(batch_docs)
-                batch_docs = []
-                if len(ret) % 1000 == 0:
-                    logger.info(f"Vectorized {len(ret)} documents...")
-        vectorize_doc(batch_docs, target_fields, bert_cls_field, vectorizer)
-        ret.extend(batch_docs)
-    return ret
-
-
-def post_documents(
-    es: "es_wrapper.ES", index: str, docs: List[Dict[str, Any]], bulk_size: int
-):
-    es.index(index, docs, bulk_size)
+from similar_text_search_ja import es as es_wrapper
+from similar_text_search_ja import index as index_base
+from similar_text_search_ja import utils, vectorizers
 
 
 def main():
@@ -88,7 +13,7 @@ def main():
     index = mlit_conf["es_index"]
 
     es = es_wrapper.ES([conf["es_url"]])
-    create_index(
+    index_base.create_index(
         es,
         mlit_conf["es_always_clear_index"],
         index,
@@ -99,10 +24,10 @@ def main():
     )
 
     vectorizer = vectorizers.JaVectorizer()
-    docs = get_documents(
+    docs = index_base.get_documents(
         mlit_conf["csv_path"], mlit_conf["target_fields"], bert_cls_field, vectorizer
     )
-    post_documents(es, index, docs, mlit_conf["es_bulk_size"])
+    index_base.post_documents(es, index, docs, mlit_conf["es_bulk_size"])
 
 
 if __name__ == "__main__":
