bbf47c95025470555897bfa1c0ec531c5614b976
Risa Nishiyama <lisa.nishiyama@gmail.com>
2020-11-21 15:37:56 +0900

    Refactor mlit.index

--- similar_text_search_ja/mlit/index.py
+++ similar_text_search_ja/mlit/index.py
@@ -1,34 +1,64 @@
+from typing import Any, Dict, List
+
 from similar_text_search_ja import es as es_wrapper
 from similar_text_search_ja import index as index_base
 from similar_text_search_ja import utils, vectorizers
 
 
-def main():
-    utils.set_root_logger()
-
+def __get_config():
     config_file = utils.get_dir().parent / "config.json"
-    conf = utils.read_json_config(config_file)
+    return utils.read_json_config(config_file)
+
+
+def __create_index(es: "es_wrapper.ES", conf: Dict[str, Any]):
     mlit_conf = conf["mlit"]
-
-    bert_cls_field = conf["bert_cls_field"]
-    index = mlit_conf["es_index"]
-
-    es = es_wrapper.ES([conf["es_url"]])
     index_base.create_index(
         es,
         mlit_conf["es_always_clear_index"],
-        index,
-        mlit_conf["date_field"],
-        mlit_conf["date_format"],
-        bert_cls_field,
-        conf["bert_vec_size"],
+        mlit_conf["es_index"],
+        {
+            mlit_conf["date_field"]: {
+                "type": "date",
+                "format": mlit_conf["date_format"],
+            },
+            conf["bert_cls_field"]: {
+                "type": "dense_vector",
+                "dims": conf["bert_vec_size"],
+            },
+        },
     )
 
-    vectorizer = vectorizers.JaVectorizer()
-    docs = index_base.get_documents(
-        mlit_conf["csv_path"], mlit_conf["target_fields"], bert_cls_field, vectorizer
+
+def __get_documents(conf: Dict[str, Any]):
+    mlit_conf = conf["mlit"]
+
+    return index_base.get_documents(
+        mlit_conf["csv_path"],
+        mlit_conf["target_fields"],
+        conf["bert_cls_field"],
+        vectorizers.JaVectorizer(),
     )
-    index_base.post_documents(es, index, docs, mlit_conf["es_bulk_size"])
+
+
+def __post_documents(
+    es: "es_wrapper.ES", docs: List[Dict[str, Any]], conf: Dict[str, Any]
+):
+    mlit_conf = conf["mlit"]
+    index_base.post_documents(
+        es, mlit_conf["es_index"], docs, mlit_conf["es_bulk_size"]
+    )
+
+
+def main():
+    utils.set_root_logger()
+
+    conf = __get_config()
+
+    es = es_wrapper.ES([conf["es_url"]])
+
+    __create_index(es, conf)
+
+    __post_documents(es, __get_documents(conf), conf)
 
 
 if __name__ == "__main__":
