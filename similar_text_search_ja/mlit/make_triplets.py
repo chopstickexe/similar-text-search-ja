3a79817cad5d4f53ba279d07f653ba4c1e031237
Risa Nishiyama <lisa.nishiyama@gmail.com>
2020-11-22 15:49:37 +0900

    Split triplets into train, dev, test

--- similar_text_search_ja/mlit/make_triplets.py
+++ similar_text_search_ja/mlit/make_triplets.py
@@ -1,9 +1,11 @@
+from pathlib import Path
 from random import randrange
 from typing import Set
 
 import numpy as np
 import pandas as pd
 from similar_text_search_ja.mlit import config
+from sklearn.model_selection import train_test_split
 
 
 def __get_documents(csv_path: str) -> pd.DataFrame:
@@ -49,6 +51,20 @@ def __generate_triplets(
     return ret_df[[t_txt_col, p_txt_col, n_txt_col]]
 
 
+def __split_train_dev_test(
+    df: pd.DataFrame, train_size: float, dev_size: float, test_size: float
+):
+    train, dev_test = train_test_split(df, train_size=train_size, random_state=4)
+    dev, test = train_test_split(
+        dev_test, train_size=dev_size / (dev_size + test_size), random_state=7
+    )
+    return train, dev, test
+
+
+def __create_dataset_dir(dataset_dir: Path):
+    dataset_dir.mkdir(parents=True, exist_ok=True)
+
+
 def __to_tsv(df: pd.DataFrame, csv_path: str):
     df.to_csv(csv_path, header=False, index=False, sep="\t")
 
@@ -61,11 +77,18 @@ def main():
     n_txt_col = "n"
     t_txt_col = "t"  # target text
 
+    dir_path = Path(mlit_conf["triplet_dataset_dir"])
+    __create_dataset_dir(dir_path)
+
     df = __get_documents(mlit_conf["csv_path"])
     df = __generate_triplets(
         df, mlit_conf["ans_field"], txt_col, t_txt_col, p_txt_col, n_txt_col
     )
-    __to_tsv(df, mlit_conf["triplet_tsv_path"])
+    train, dev, test = __split_train_dev_test(df, 0.4, 0.3, 0.3)
+
+    __to_tsv(train, str(dir_path / "train.tsv"))
+    __to_tsv(dev, str(dir_path / "dev.tsv"))
+    __to_tsv(test, str(dir_path / "test.tsv"))
 
 
 if __name__ == "__main__":
