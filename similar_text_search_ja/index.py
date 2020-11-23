284dd821307dca7f4bb2781b70e6c750ec144084
Risa Nishiyama <lisa.nishiyama@gmail.com>
2020-11-23 19:50:43 +0900

    Use dataset-specific settings in index

--- similar_text_search_ja/index.py
+++ similar_text_search_ja/index.py
@@ -1,12 +1,15 @@
 import csv
 import logging
+import sys
 from pathlib import Path
 from typing import Any, Dict, List
 
-from similar_text_search_ja import csv_parser
+from similar_text_search_ja import config
 from similar_text_search_ja import es as es_wrapper
 from similar_text_search_ja import utils
-from similar_text_search_ja.vectorizers import BaseVectorizer, HuggingfaceVectorizer
+from similar_text_search_ja.config import Config
+from similar_text_search_ja.vectorizers import (BaseVectorizer,
+                                                HuggingfaceVectorizer)
 
 
 def create_index(
@@ -27,40 +30,6 @@ def create_index(
         index,
         body={"mappings": {"properties": fields}},
     )
-
-
-def get_documents(
-    csv_path: str,
-    target_fields: List[str],
-    vector_field: str,
-    vectorizer: "BaseVectorizer",
-) -> List[Dict[str, Any]]:
-    """Read documents from a csv file and add a dense vector to each doc
-
-    Args:
-        csv_path (str): CSV path
-        target_fields (List[str]): Text fields
-        vector_field (str): Field to store dense vectors representing the text
-        vectorizer (BaseVectorizer): Vectorizer instance
-
-    Returns:
-        List[Dict[str, Any]]: Documents with dense vectors
-    """
-    logger = logging.getLogger(__name__)
-    ret = []
-    with csv_parser.CsvParser(csv_path) as parser:
-        batch_docs = []
-        for doc in parser:
-            batch_docs.append(doc)
-            if len(batch_docs) == 32:
-                _vectorize_doc(batch_docs, target_fields, vector_field, vectorizer)
-                ret.extend(batch_docs)
-                batch_docs = []
-                if len(ret) % 1000 == 0:
-                    logger.info(f"Vectorized {len(ret)} documents...")
-        _vectorize_doc(batch_docs, target_fields, vector_field, vectorizer)
-        ret.extend(batch_docs)
-    return ret
 
 
 def doc_generator(csv_path: str, batch_size: int):
@@ -87,10 +56,10 @@ def add_vectors(
 def get_stats(
     docs: List[Dict[str, Any]],
     vectorizer: "BaseVectorizer",
-    conf: Dict[str, Any],
+    target_fields: List[str],
 ):
     logger = logging.getLogger(__name__)
-    target_fields = conf["mlit"]["target_fields"]
+    target_fields = target_fields
     sum_tokens = 0
     sum_chars = 0
     for doc in docs:
@@ -135,55 +104,58 @@ def post_documents(
     es.index(index, docs, bulk_size)
 
 
-def create_report_dir(report: Path):
-    reports_dir = report.parent
-    reports_dir.mkdir(parents=True, exist_ok=True)
-
-
 def print_summary(report: Path, avg_tokens: float, avg_chars: float):
     with open(str(report), mode="w") as f:
         f.write(f"Average char length = {avg_chars: .3f}\n")
         f.write(f"Average token length = {avg_tokens: .3f}\n")
 
 
+def __get_es_index_settings(
+    base: Dict[str, Any], embedding_field_name: str, embedding_dim: int
+):
+    base[embedding_field_name] = {
+        "type": "dense_vector",
+        "dims": embedding_dim,
+    }
+    return base
+
+
 def main():
-    """Example usage"""
     utils.set_root_logger()
     logger = logging.getLogger(__name__)
 
-    es = es_wrapper.ES(["es01:9200"])
+    dataset = sys.argv[1]
+    conf = Config(dataset)
 
-    vectorizer_config = {
-        "tokenizer_name_or_path": "cl-tohoku/bert-base-japanese-whole-word-masking",
-        "model_name_or_path": "cl-tohoku/bert-base-japanese-whole-word-masking",
-    }
-    vectorizer = HuggingfaceVectorizer.create(vectorizer_config)
-    fields = {
-        "受付日": {
-            "type": "date",
-            "format": "yyyy年MM月dd日",
-        },
-        "bert_cls_vec": {
-            "type": "dense_vector",
-            "dims": vectorizer.word_embedding_dim,
-        },
-    }
-    index = "test"
-    create_index(es=es, clear_index=True, index=index, fields=fields)
+    config.create_dir(conf.report_dir)
+
+    es = es_wrapper.ES([conf.es_url])
+
+    vectorizer = HuggingfaceVectorizer.create(conf.vect_config)
+    field_settings = __get_es_index_settings(
+        conf.es_base_index_settings,
+        conf.es_embedding_field,
+        vectorizer.word_embedding_dim,
+    )
+    create_index(
+        es=es, clear_index=True, index=conf.es_index_name, fields=field_settings
+    )
 
     docs = []
-    for batch in doc_generator("data/mlit/raw/mlit.sample.csv", batch_size=100):
+    for batch in doc_generator(conf.raw_csv_path, batch_size=conf.vect_batch_size):
         logger.info(f"Vectorize {len(batch)} documents...")
         add_vectors(
             batch,
-            target_fields=["申告内容の要約"],
-            vector_field="bert_cls_vec",
+            target_fields=conf.target_fields,
+            vector_field=conf.es_embedding_field,
             vectorizer=vectorizer,
         )
         docs.extend(batch)
 
-    post_documents(es=es, index=index, docs=docs, bulk_size=1000)
-    es.delete_index(index)
+    avg_tokens, avg_chars = get_stats(docs, vectorizer, conf.target_fields)
+    print_summary(conf.report_dir / "index.txt", avg_tokens, avg_chars)
+
+    post_documents(es=es, index=conf.es_index_name, docs=docs, bulk_size=1000)
 
 
 if __name__ == "__main__":
